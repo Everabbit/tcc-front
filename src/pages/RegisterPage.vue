bf66d82ef7d462a5689b49018169f3031a7dea93
Everabbit <wendeldelemoscoelho@gmail.com>
2025-03-26 00:14:03 -0300

    iniciado definitivamente o front do sistema

---
+++ src/pages/RegisterPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card flat class="row justify-center" style="min-width: 50vw">
+      <div class="col-6 q-pa-lg">
+        <q-card flat>
+          <q-card-section>
+            <span class="text-primary flex justify-center items-center text-h5">
+              <q-icon size="lg" name="mdi-fireplace" class="q-mr-md" /> <b>TaskForge</b>
+            </span>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-6">
+        <q-img src="../assets/auth_image.png" class="rounded-borders" style="position: relative">
+          <div class="absolute-full flex items-center justify-center column">
+            <span class="text-white text-h6">Gerencie seus projetos com eficiência</span>
+            <span class="text-white text-body1 text-weight-light text-center q-mt-md q-px-xl"
+              >Organize tarefas, colabore com equipes e acompanhe o progresso de seus projetos em um
+              só lugar.</span
+            >
+          </div>
+        </q-img>
+      </div>
+    </q-card>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const nome = ref<string>('');
+    const email = ref<string>('');
+    const senha = ref<string>('');
+    const confirmarSenha = ref<string>('');
+    return {
+      nome,
+      email,
+      senha,
+      confirmarSenha,
+    };
+  },
+};
+</script>
